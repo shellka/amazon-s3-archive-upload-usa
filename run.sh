932be3e6024fe6ba93c913f2b0ccccfc57552f25
Leonid Brish <leonid.v.brish@gmail.com>
2016-11-19 18:18:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ if [ ! -n "$WERCKER_S3_ARCHIVE_UPLOAD_SECRET" ]; then
   exit 1
 fi
 
-if [ ! -n "$WERCKER_S3_ARCHIVE_UPLOAD_BUCKET" ]; then
+if [ ! -n "$WERCKER_S3_ARCHIVE_UPLOAD_BUCKET3" ]; then
   #set default bucket
-  export WERCKER_S3_ARCHIVE_UPLOAD_BUCKET="wercker-deployments"
+  export WERCKER_S3_ARCHIVE_UPLOAD_BUCKET3="wercker-deployments"
 fi
 
-if [ ! -n "$WERCKER_S3_ARCHIVE_UPLOAD_REGION" ]; then
+if [ ! -n "$WERCKER_S3_ARCHIVE_UPLOAD_REGION3" ]; then
   error 'Please specify region'
   exit 1
 fi
@@ -31,13 +31,13 @@ aws --version
 mkdir -p $HOME/.aws
 echo '[default]' > $HOME/.aws/config
 echo 'output = json' >> $HOME/.aws/config
-echo "region = $WERCKER_S3_ARCHIVE_UPLOAD_REGION" >> $HOME/.aws/config
+echo "region = $WERCKER_S3_ARCHIVE_UPLOAD_REGION3" >> $HOME/.aws/config
 echo "aws_access_key_id = $WERCKER_S3_ARCHIVE_UPLOAD_KEY" >> $HOME/.aws/config
 echo "aws_secret_access_key = $WERCKER_S3_ARCHIVE_UPLOAD_SECRET" >> $HOME/.aws/config
 
 export AMAZON_ACCESS_KEY_ID=$WERCKER_S3_ARCHIVE_UPLOAD_KEY
 export AMAZON_SECRET_ACCESS_KEY=$WERCKER_S3_ARCHIVE_UPLOAD_SECRET
-export AWS_DEFAULT_REGION=$WERCKER_S3_ARCHIVE_UPLOAD_REGION
+export AWS_DEFAULT_REGION=$WERCKER_S3_ARCHIVE_UPLOAD_REGION3
 export AWS_APP_VERSION_LABEL=$WERCKER_GIT_COMMIT
 export AWS_APP_FILENAME=$AWS_APP_VERSION_LABEL.zip
 
@@ -51,4 +51,4 @@ if [ ! -f $AWS_APP_FILENAME ]; then
 fi
 
 #aws configure set default.s3.signature_version s3v4
-aws s3 cp --acl private $AWS_APP_FILENAME s3://$WERCKER_S3_ARCHIVE_UPLOAD_BUCKET
+aws s3 cp --acl private $AWS_APP_FILENAME s3://$WERCKER_S3_ARCHIVE_UPLOAD_BUCKET3
